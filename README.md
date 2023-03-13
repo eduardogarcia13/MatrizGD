de74ad33e50a6929cece04dd8946a78e6f74bc20
eduardogarciar13@gmail.com <luis.garcia@imss.gob.mx>
2023-03-13 15:41:38 -0600

    ModeloIncapacidades

--- README.md
+++
@@ -1,2 +0,0 @@
-# MatrizGD
-MatrizGD
---
+++ Readme_RN.sql
@@ -0,0 +1,7 @@
+--MatrizGD
+--A: Insertar datos.
+INSERT INTO nombreTabla(column0, column0)
+VALUES (valu1, value2);
+--B: Baja de datos.
+
+--C: Cambios de datos.
